e3f04161fb9cd39f6a4803346426b6aaf424eeb3
Cristian Uncos <uncosc@gmail.com>
2022-07-03 16:27:37 -0300

    hasta el punto 5

--- js/variables.js
+++ js/variables.js
@@ -7,3 +7,11 @@ document.write("<p> Hello World </p>");
 // 3) SUMA
 document.write("<p>Resultado de 5+3:" + (3+5) + "</p>");
 
+// 4) PEDIDO DE USUARIO
+Usuario = prompt("Ingrese su nombre de usuario");
+document.write("Hola " + Usuario );
+ 
+// 5) SSUMA DE NUMEROS
+PrimerNum = parseInt(prompt("Indique su numero"))
+SegundoNum = parseInt(prompt("Indique su segundo numero"))
+document.write("<p>Resultado:"+(PrimerNum+SegundoNum)+"</p>")
