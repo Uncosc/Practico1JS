6072d7bbb21463510729132878d93077dd80593d
Cristian Uncos <uncosc@gmail.com>
2022-07-03 15:11:17 -0300

    hasta el punto 3

---
+++ js/variables.js
@@ -0,0 +1,9 @@
+// 1) ALERTA
+alert("Nos fuimos mundiales xd JAAJJAJAJ");
+
+// 2) MENSAJE
+document.write("<p> Hello World </p>");
+
+// 3) SUMA
+document.write("<p>Resultado de 5+3:" + (3+5) + "</p>");
+
