c99c5fdb18735db9b3bd538bfce5df5af107450a
Cristian Uncos <uncosc@gmail.com>
2022-07-06 20:00:43 -0300

    se relaiza hasta el ejercicio 7 y se acomoda

--- js/Parte1TP.js
+++ js/Parte1TP.js
@@ -15,3 +15,5 @@ document.write("Hola " + Usuario );
 let PrimerNum = parseInt(prompt("Indique su numero"))
 let SegundoNum = parseInt(prompt("Indique su segundo numero"))
 document.write("<p>Resultado:"+(PrimerNum+SegundoNum)+"</p>")
+
+
---
+++ js/Parte2TP.js
@@ -0,0 +1,17 @@
+//-----------------2° parte del practico-------------------
+
+// 6) MOSTRAR EL NUMERO MAYOR
+
+/*
+let Numero1 = parseInt(prompt("indiique un numero"))
+let Numero2 = parseInt(prompt("indiique un numero"))
+
+if(Numero1>Numero2){
+    document.write("el primeer numero es el mayor, numero ingresado" + Numero1);
+} else{
+    document.write("El segundo numero es el mayor, numero ingresado:" + Numero2);
+}
+
+*/
+
+// 7)
