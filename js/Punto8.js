0d4dad6d5d0626934e857cdcaa79cf8877d80be5
Cristian Uncos <uncosc@gmail.com>
2022-07-06 21:06:51 -0300

    hasta el punto 8

---
+++ js/Punto8.js
@@ -0,0 +1,9 @@
+// 8) numero divisible en dos
+let NumeroDiv = parseInt(prompt("Indique su numero y le dire si es divisible en 2"));
+let Divisor = 2
+
+if (NumeroDiv % Divisor === 0){
+    document.write("Si es divisible campeon")
+} else{
+    document.write("El numero iungresado no es divisible en 2")
+}
